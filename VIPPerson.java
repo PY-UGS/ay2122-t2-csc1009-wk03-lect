7b5850499d433817fdba070b4287609522b7cfdd
Peter YAU <9554866+petercyyau@users.noreply.github.com>
2022-01-23 23:10:27 +0800

    Create VIPPerson.java

---
+++ VIPPerson.java
@@ -0,0 +1,25 @@
+public class VIPPerson extends Person{
+
+    private String grade; //m-class Million, b-class Billion
+
+    public VIPPerson(String name) {
+        super(name);
+    }
+
+    public VIPPerson(String name, String address) {
+        super(name, address);
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public void sayGreetings(){
+        System.out.println("Thank you for being our valuable customer, VIP " + super.name);
+    }
+
+}
