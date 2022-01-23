dff42605311a4b2a107a3c77add061db37ae9308
Peter YAU <9554866+petercyyau@users.noreply.github.com>
2022-01-23 23:09:36 +0800

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,54 @@
+public class Person {
+
+    protected String name;
+    private String address;
+    private Account account;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Account getAccount() {
+        return this.account;
+    }
+
+//    public void printAccountDetail() {
+//        System.out.println("Account number is: " + this.account.accountNumber);
+//        System.out.println("Is this a Saving Account? " + this.account.isSavingAccount);
+//    }
+
+    public void printAccountDetailSecondMethod() {
+        System.out.println("Account number is: " + this.account.getAccountNumber());
+        System.out.println("Is this a Saving Account? " + this.account.isSavingAccount());
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public void sayGreetings(){
+        System.out.println("Thank you for being our customer, " + this.name);
+
+    }
+}
